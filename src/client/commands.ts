9fa4992a7f549351c97d17b1ff95c94970e74bb3
Acid147 <norman.rauschen@web.de>
2019-06-29 17:41:12 +0200

    Create TypeScript project

---
+++ src/client/commands.ts
@@ -0,0 +1,29 @@
+/**
+ * Run current file.
+ */
+export function run() { }
+
+/**
+ * Show help info.
+ */
+export function help() { }
+
+/**
+ * Show local paths info.
+ */
+export function path() { }
+
+/**
+ * Test current file.
+ */
+export function testFile() { }
+
+/**
+ * Test current package.
+ */
+export function testPackage() { }
+
+/**
+ * Upload and share current code to V playground.
+ */
+export function playground() { }
