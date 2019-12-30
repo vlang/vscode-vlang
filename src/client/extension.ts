3d353e8a6529942a4556f5ada6ca7cc67b0dfc9e
0x9ef <0x9ef@tutanota.com>
2019-12-30 17:28:45 +0200

    Version 0.0.9

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -1,21 +1,22 @@
 import * as vscode from 'vscode';
 import * as commands from './commands';
-import { registerFormatter } from './format'
+import { registerFormatter } from './format';
 /**
  * This method is called when the extension is activated.
  * @param context The extension context is a collection of utilities private to
  * the extension.
  */
 export function activate(context: vscode.ExtensionContext) {
-  let run = vscode.commands.registerCommand('v.run', commands.run);
-  let prod = vscode.commands.registerCommand('v.prod', commands.prod);
-  let help = vscode.commands.registerCommand('v.help', commands.help);
-  let ver = vscode.commands.registerCommand('v.ver', commands.ver);
-  let path = vscode.commands.registerCommand('v.path', commands.path);
-  let testFile = vscode.commands.registerCommand('v.test.file', commands.testFile);
-  let testPackage = vscode.commands.registerCommand('v.test.package', commands.testPackage);
-  let playground = vscode.commands.registerCommand('v.playground', commands.playground);
-  registerFormatter();
+	let run = vscode.commands.registerCommand('v.run', commands.run);
+	let prod = vscode.commands.registerCommand('v.prod', commands.prod);
+	let help = vscode.commands.registerCommand('v.help', commands.help);
+	let ver = vscode.commands.registerCommand('v.ver', commands.ver);
+	let path = vscode.commands.registerCommand('v.path', commands.path);
+	let testFile = vscode.commands.registerCommand('v.test.file',commands.testFile);
+	let testPackage = vscode.commands.registerCommand('v.test.package',commands.testPackage);
+    let playground = vscode.commands.registerCommand('v.playground',commands.playground);
+    
+	registerFormatter();
 }
 
 /**
--- src/client/format.ts
+++ src/client/format.ts
@@ -1,43 +1,48 @@
 import * as vscode from 'vscode';
-import * as childProcess from 'child_process'
+import * as childProcess from 'child_process';
 import { fullDocumentRange } from './utils';
 
 function format(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
-  return new Promise((resolve, reject) => {
-    // Create vfmt command
-    const vfmtArgs: string = vscode.workspace.getConfiguration('v.format').get('args') || '';
-    const cmd = `v fmt ${vfmtArgs} ${document.fileName}`;
+	return new Promise((resolve, reject) => {
+		// Create `vfmt` command with entered arguments.
+		const vfmtArgs: string =
+			vscode.workspace.getConfiguration('v.format').get('args') || '';
+        const cmd = `v fmt ${vfmtArgs} ${document.fileName}`;
 
-    console.log(`Running ${cmd}...`);
+        // Create new `callback` function for 
+		function callback(
+			error: childProcess.ExecException,
+			stdout: string,
+			stderr: string
+		) {
+            const isErr = error !== null;
+			if (isErr) {
+                const errMessage = `Cannot format due to the following errors: ${stderr}`;
+				vscode.window.showErrorMessage(errMessage);
+				return reject(errMessage);
+            }
+			return resolve([vscode.TextEdit.replace(fullDocumentRange(document), stdout)]);
+		}
 
-    function callback(error: childProcess.ExecException, stdout: string, stderr: string) {
-      if (error !== null) {
-        const message = `Cannot format due to the following errors: ${stderr}`;
-        vscode.window.showErrorMessage(message);
-        return reject(message);
-      }
-      console.log('Formatting complete');
-      return resolve([vscode.TextEdit.replace(fullDocumentRange(document), stdout)]);
-    }
-    
-    // TODO: vscode.workspace.rootPath is deprecated. 
-    childProcess.exec(cmd, { cwd: vscode.workspace.rootPath }, callback);
-  });
+        // TODO: vscode.workspace.rootPath is deprecated.
+        console.log(`Running ${cmd}...`);
+		childProcess.exec(cmd, { cwd: vscode.workspace.rootPath }, callback);
+	});
 }
 
 export function registerFormatter() {
-  const formatterEnabled = vscode.workspace.getConfiguration('v.format').get('enable');
-  if (!formatterEnabled) {
-    console.log('Formatter not enabled');
-    return;
-  }
-
-  const provider: vscode.DocumentFormattingEditProvider = {
-    provideDocumentFormattingEdits( document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
-        return document.save().then(() => format(document));
-      }
+	const formatterEnabled = vscode.workspace.getConfiguration('v.format').get('enable');
+	if (!formatterEnabled) {
+		console.log('Formatter not enabled!');
+		return;
     }
-
-  // Register the vfmt formatter
-  vscode.languages.registerDocumentFormattingEditProvider('v', provider);
+    
+	const provider: vscode.DocumentFormattingEditProvider = {
+		provideDocumentFormattingEdits(
+			document: vscode.TextDocument
+		): Thenable<vscode.TextEdit[]> {
+			return document.save().then(() => format(document));
+		}
+    };
+	vscode.languages.registerDocumentFormattingEditProvider('v', provider);
 }
