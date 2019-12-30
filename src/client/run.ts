9fdf380211bfcd0b14f55db912cb6fb75383b889
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2019-12-30 21:06:20 +0800

    cosmetic fix 😁

--- src/client/run.ts
+++ src/client/run.ts
@@ -2,24 +2,17 @@ import * as vscode from 'vscode';
 
 let vRunTerm: vscode.Terminal = null
 
-function runTerm(term: vscode.Terminal, cmd: string) {
-    term.show()
-    term.sendText(cmd)
-}
-
 export function vrun() {
     const cmd = 'v run ' + vscode.window.activeTextEditor.document.fileName
-    
-    vscode.window.activeTextEditor.document.save()
-    
-    if (!vRunTerm) {
-        vRunTerm = vscode.window.createTerminal(cmd)
-        runTerm(vRunTerm, cmd)
-    } else {
-        runTerm(vRunTerm, cmd)
-    }
 
-    vscode.window.onDidCloseTerminal((term)=> {
-        if (term.name == cmd) vRunTerm = null 
+    vscode.window.activeTextEditor.document.save()
+
+    if (!vRunTerm) vRunTerm = vscode.window.createTerminal('V')
+
+    vRunTerm.show()
+    vRunTerm.sendText(cmd)
+
+    vscode.window.onDidCloseTerminal(term => {
+        if (term.name == 'V') vRunTerm = null
     })
 }
