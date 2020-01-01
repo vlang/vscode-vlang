a811a63a11a6d6d1b8ecc59eb927e0070edc2c74
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-01-02 01:52:55 +0800

    replace deprecated API with new API

--- src/client/format.ts
+++ src/client/format.ts
@@ -9,7 +9,7 @@ function format(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
 			vscode.workspace.getConfiguration('v.format').get('args') || '';
 		const cmd = `v fmt ${vfmtArgs} ${document.fileName}`;
 
-		// Create new `callback` function for 
+		// Create new `callback` function for
 		function callback(
 			error: childProcess.ExecException,
 			stdout: string,
@@ -24,9 +24,8 @@ function format(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
 			return resolve([vscode.TextEdit.replace(fullDocumentRange(document), stdout)]);
 		}
 
-		// TODO: vscode.workspace.rootPath is deprecated.
 		console.log(`Running ${cmd}...`);
-		childProcess.exec(cmd, { cwd: vscode.workspace.rootPath }, callback);
+		childProcess.exec(cmd, { cwd: vscode.workspace.workspaceFolders[0].name }, callback);
 	});
 }
 
@@ -34,7 +33,7 @@ export function registerFormatter() {
 	const provider: vscode.DocumentFormattingEditProvider = {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
 			return document.save().then(() => format(document));
-		},
+		}
 	};
 	vscode.languages.registerDocumentFormattingEditProvider('v', provider);
 }
