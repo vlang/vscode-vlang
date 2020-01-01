39a8c55c7af2d788bc1b55a5abae6c8792b3446c
Acid147 <norman.rauschen@web.de>
2020-01-01 19:53:17 +0100

    Implement "Show version info"

--- src/client/commands.ts
+++ src/client/commands.ts
@@ -1,3 +1,5 @@
+import * as vscode from 'vscode';
+import * as childProcess from 'child_process';
 import { vrun } from './run';
 
 /**
@@ -10,34 +12,43 @@ export function run() {
 /**
  * Build an optimized executable from current file.
  */
-export function prod() {}
+export function prod() { }
 
 /**
  * Show help info.
  */
-export function help() {}
+export function help() { }
 
 /**
  * Show version info.
  */
-export function ver() {}
+export function ver() {
+	childProcess.exec('v -v', (err, stdout, stderr) => {
+		if (err) {
+			vscode.window.showErrorMessage('Unable to get the version number. Is V installed correctly?');
+			return;
+		}
+
+		vscode.window.showInformationMessage(stdout);
+	});
+}
 
 /**
  * Show local paths info.
  */
-export function path() {}
+export function path() { }
 
 /**
  * Test current file.
  */
-export function testFile() {}
+export function testFile() { }
 
 /**
  * Test current package.
  */
-export function testPackage() {}
+export function testPackage() { }
 
 /**
  * Upload and share current code to V playground.
  */
-export function playground() {}
+export function playground() { }
