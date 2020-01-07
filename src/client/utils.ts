77c3d069736391c08eaf38b96342e7ca09cc18be
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-01-08 01:26:09 +0800

    Improve utils

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -1,4 +1,4 @@
-import { Range, TextDocument, window, workspace } from 'vscode';
+import { Range, TextDocument, window, workspace, WorkspaceConfiguration } from 'vscode';
 
 const defaultCommand = 'v';
 
@@ -8,17 +8,24 @@ export function fullDocumentRange(document: TextDocument): Range {
 }
 
 export function getVExecCommand(args: string): string {
-	const config = workspace.getConfiguration('v');
-	const vPath = config.get('pathToExecutableFile', '') || defaultCommand;
+	const config = getVConfig();
+	const vPath = config.get('pathToExecutableFile', defaultCommand)
 
 	return `${vPath} ${args}`;
+}
+
+export function getVConfig(document?: TextDocument): WorkspaceConfiguration {
+	let vConfig = workspace.getConfiguration('v');
+	if (document) vConfig = workspace.getConfiguration('v', document.uri);
+	return vConfig;
 }
 
 export function getCurrentFilePath(): string {
 	return window.activeTextEditor.document.fileName;
 }
 
-export function getCwd() {
-	const folder = workspace.workspaceFolders[0];
+export function getCwd(document?: TextDocument): string {
+	let folder = workspace.workspaceFolders[0];
+	if (document) folder = workspace.getWorkspaceFolder(document.uri);
 	return folder.uri.fsPath;
 }
