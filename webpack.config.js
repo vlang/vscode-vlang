45496d31e4f0f491167a9c8abd5707104ed33329
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-05-10 19:47:36 +0800

    Remove unnecessary comments

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,9 @@ const path = require("path");
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-	target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-
-	entry: "./src/client/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+	target: "node",
+	entry: "./src/client/extension.ts",
 	output: {
-		// the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
 		path: path.resolve(__dirname, "out"),
 		filename: "extension.js",
 		libraryTarget: "commonjs2",
@@ -17,10 +15,9 @@ const config = {
 	},
 	devtool: "source-map",
 	externals: {
-		vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+		vscode: "commonjs vscode",
 	},
 	resolve: {
-		// support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
 		extensions: [".ts", ".js"],
 	},
 	module: {
