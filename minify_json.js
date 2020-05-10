ca8e3a6793e7c248352751612c6b25a25a78e51b
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2020-05-11 00:28:08 +0800

    Fix up linter issue & Improve extension load time (#146)
    * linter: Check main function also
    
    * Add webpack to optimize load performance
    
    * Optimize load time by minifying json files
    
    * Remove unnecessary comments
    
    * Use or operator instead &&

---
+++ minify_json.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+"use strict";
+// @ts-check
+
+const { exec } = require("child_process");
+const { writeFileSync } = require("fs");
+const { resolve } = require("path");
+
+const jsonFiles = [
+	"./syntaxes/v.tmLanguage.json",
+	"./language-configuration.json",
+	"./snippets/snippets.json",
+];
+
+jsonFiles.forEach((jsonFile) => {
+	const absolutePath = resolve(__dirname, jsonFile);
+	exec("npx json-minify " + absolutePath, (error, stdout) => {
+		if (!!error) throw error;
+		const outputFile = absolutePath;
+		// console.log(outputFile);
+		writeFileSync(outputFile, stdout);
+	});
+});
