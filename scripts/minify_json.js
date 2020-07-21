3ac42592f100d5b5d145175e740a52563c07c92b
Ryan Willis <code@ryanwillis.com>
2020-07-20 18:53:10 -0700

    Release 0.1.3

--- scripts/minify_json.js
+++ scripts/minify_json.js
@@ -7,17 +7,16 @@ const { writeFileSync } = require("fs");
 const { resolve } = require("path");
 
 const jsonFiles = [
-	"../syntaxes/v.tmLanguage.json",
-	"../language-configuration.json",
-	"../snippets/snippets.json",
+	"v.tmLanguage.json",
+	"language-configuration.json",
+	"snippets.json",
 ];
 
 jsonFiles.forEach((jsonFile) => {
-	const absolutePath = resolve(__dirname, jsonFile);
-	exec("npx json-minify " + absolutePath, (error, stdout) => {
+	const inputFile  = resolve(__dirname + '/../conf/', jsonFile);
+	const outputFile = resolve(__dirname + '/../out/', jsonFile);
+	exec("npx json-minify " + inputFile, (error, stdout) => {
 		if (!!error) throw error;
-		const outputFile = absolutePath;
-		// console.log(outputFile);
 		writeFileSync(outputFile, stdout);
 	});
 });
