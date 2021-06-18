c0cee822567d28e5512297e11f21b68244165749
Lukas Neubert <serkonda@protonmail.com>
2021-06-18 15:19:20 +0200

    unit tests for syntax highlighting
    close #233
    
    For now I added only a test for number separators. More tests should be added with each upcoming change.

---
+++ syntaxes/tests/number_separator.v
@@ -0,0 +1,11 @@
+// SYNTAX TEST "source.v" "_ number separators"
+_ := 1_000_000
+//   ^^^^^^^^^ constant.numeric.integer.v
+_ := 3_122.55
+//   ^^^^^^^^ constant.numeric.float.v
+_ := 0xF_F
+//   ^^^^^ constant.numeric.hex.v
+_ := 0o17_3
+//   ^^^^^^ constant.numeric.octal.v
+_ := 0b0_11
+//   ^^^^^^ constant.numeric.binary.v
