4594bace8848d89cd2934c1e72d68a895ec39033
Lukas Neubert <serkonda@protonmail.com>
2021-06-23 12:30:51 +0200

    migrate from webpack to esbuild (#236)

---
+++ scripts/build.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+//@ts-check
+'use strict';
+
+const esbuild = require('esbuild');
+
+const isWatch = process.argv.includes('--watch');
+const isProd = process.env.NODE_ENV === 'production';
+
+esbuild.build({
+	platform: 'node',
+	entryPoints: ['./src/extension.ts'],
+	outdir: './out',
+	external: ["vscode"],
+	format: 'cjs',
+	sourcemap: 'external',
+	bundle: true,
+	minify: isProd,
+	watch: {
+		onRebuild(error, result) {
+			if (error) console.error('watch build failed')
+			else console.log('watch build succeeded')
+		},
+	},
+}).then(res => {
+	if (!isWatch) {
+		res.stop();
+	}
+});
