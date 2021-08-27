b5b1da16809c291c997c1ff64c297b9253d188c0
Lukas Neubert <serkonda@protonmail.com>
2021-08-27 20:14:57 +0200

    expand test

--- syntaxes/tests/comma.v
+++ syntaxes/tests/comma.v
@@ -1,3 +1,5 @@
 // SYNTAX TEST "source.v" "comma"
-[ 1, 2, 3, 4, 5 ]
+ [1, 2, 3, 4]
+// ^ punctuation.separator.comma.v
 //    ^ punctuation.separator.comma.v
+//       ^ punctuation.separator.comma.v
