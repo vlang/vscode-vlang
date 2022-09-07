fc28b62b980412a6544ad9e75d28203acdd3727d
awgh <awgh@awgh.org>
2022-09-07 09:29:11 -0700

    Fixes an issue where v could not be executed from the plugin at all because it was using "execFile" and not "exec", which does not work on Windows.
    See Also:
    https://stackoverflow.com/questions/46445805/exec-vs-execfile-nodejs

--- src/exec.ts
+++ src/exec.ts
@@ -1,6 +1,6 @@
 import { window, Terminal } from 'vscode';
 import { getVExecCommand, getCwd } from './utils';
-import cp, { ExecException, execFile } from 'child_process';
+import cp, { exec, ExecException, execFile } from 'child_process';
 
 type ExecCallback = (error: ExecException | null, stdout: string, stderr: string) => void;
 
@@ -27,8 +27,9 @@ export function execV(args: string[], callback: ExecCallback): void {
 	const vexec = getVExecCommand();
 	const cwd = getCwd();
 
-	// console.log(`Executing ${vexec} ${args.join(" ")} on ${cwd}`);
-	execFile(vexec, args, { cwd }, (err, stdout, stderr) => {
+	// void window.showErrorMessage(`Executing ${vexec} ${args.join(" ")} on ${cwd}`);
+	
+	exec(vexec+" "+args.join(" "), (err, stdout, stderr) => {
 		callback(err, stdout, stderr);
 	});
 }
