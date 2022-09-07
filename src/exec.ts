5b65f1ef877ff20b220aad485a3e073c2402b628
awgh <awgh@awgh.org>
2022-09-07 10:14:41 -0700

    Removing execFile import.

--- src/exec.ts
+++ src/exec.ts
@@ -1,6 +1,6 @@
 import { window, Terminal } from 'vscode';
 import { getVExecCommand, getCwd } from './utils';
-import cp, { exec, ExecException, execFile } from 'child_process';
+import cp, { exec, ExecException } from 'child_process';
 
 type ExecCallback = (error: ExecException | null, stdout: string, stderr: string) => void;
 
